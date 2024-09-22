927cf163db7cdaf196ea55d26d75a2b989fdbc24
MankGarox <almaasrakan@gmail.com>
2024-09-22 22:28:50 +0700

    Delete 23148_RanggaAlmaasRPP_Praktikum_Tugas1 directory

--- 23148_RanggaAlmaasRPP_Praktikum_Tugas1/Latihan Nomer 2/KonversiSuhu.java
+++
@@ -1,9 +0,0 @@
-public class KonversiSuhu {
-    public double celciusToFahrenheit(double celcius) {
-        return (celcius * 9/5) + 32;
-    }
-
-    public double celciusToReamur(double celcius) {
-        return celcius * 4/5;
-    }
-}
--- 23148_RanggaAlmaasRPP_Praktikum_Tugas1/Latihan Nomer 2/KonversiSuhuKedua.java
+++
@@ -1,5 +0,0 @@
-public class KonversiSuhuKedua extends KonversiSuhu {
-    public double fahrenheitToReamur(double fahrenheit) {
-        return (fahrenheit - 32) * 4/9;
-    }
-}
